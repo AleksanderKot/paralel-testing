70d567af0a0aec3d7ee8fc4bc230aed925380cf5
AleksanderKot <aleksander.kot@ttpsc.com>
2025-08-13 15:28:34 +0200

    added logvariables

---
+++ resources/ENVs/logvariables.py
@@ -0,0 +1,8 @@
+YT_LOGIN = ''
+YT_PASSWORD = ''
+PRESTA_LOGIN = ''
+PRESTA_PASSWORD = ''
+WEBGOAT_LOGIN = ''
+WEBGOAT_PASSWORD = ''
+
+#  pabot --artifacts png,jpg --artifactsinsubfolders --outputdir results tests/webgoat tests/presta
